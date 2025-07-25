b639d46d722a47fc7a8f3f6ff0931e497cb6a49e
Halmuhammet <hallysh9@gmail.com>
2025-07-25 17:58:02 +0500

    Initialize

--- 1.6-Unicafe/src/App.jsx
+++ 1.6-Unicafe/src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const FeedbackButtons = ({action, label}) => {
+  return(
+    <div>
+      <button onClick = {action}>
+        {label}
+      </button>
+    </div>
   )
 }
 
-export default App
+const Statistics = ({good, neutral, bad}) =>{
+
+  return(
+    <div>
+      <h1>Statistics</h1>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodButton = () =>{
+    setGood(good + 1);
+  }
+  const handleNeutralButton = () =>{
+    setNeutral(neutral + 1);
+  }
+  const handleBadButton = () =>{
+    setBad(bad + 1);
+  }
+  return(
+    <div>
+      <h1>Give a Feedback</h1>
+      <FeedbackButtons 
+        action = {handleGoodButton}
+        label = {"good"}
+      />
+      <FeedbackButtons 
+        action = {handleNeutralButton}
+        label = {"neutral"}
+      />
+      <FeedbackButtons 
+        action = {handleBadButton}
+        label = {"bad"}
+      />
+      <Statistics 
+        good = {good}
+        neutral = {neutral}
+        bad = {bad}
+      />
+    </div>
+  )
+}
+
+export default App;
